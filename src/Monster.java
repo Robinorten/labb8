704a8a0c70170ca0963013d201779f24141e2c9d
Mats-coder <mats.andersson422@gmail.com>
2019-12-18 12:25:46 +0100

    Added monstercclass stuff

--- src/Monster.java
+++ src/Monster.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ExecutionException;
+
 public class Monster{
     public int healthPoints;
     Position currentPos,previousPos;
@@ -5,33 +7,34 @@ public class Monster{
     public Monster(TowerDefenceLevel level){
         this.healthPoints = 10;
         this.level = level;
-        this.currentPos = new Position(1,0);
+        this.currentPos = new Position(level.startRow,level.startCol);
     }
 
     public Position getCurrentPos() {
         return currentPos;
     }
 
-    public Position move(){
-        if (level.checkPassable(currentPos.row,currentPos.col+1)){
-            previousPos = currentPos;
-            currentPos.col = currentPos.col+1;
-            return currentPos;
-        } else if (level.checkPassable(currentPos.row-1,currentPos.col)){
-            previousPos = currentPos;
-            currentPos.row = currentPos.row-1;
-            return currentPos;
-        } else if (level.checkPassable(currentPos.row+1,currentPos.col)){
-            previousPos = currentPos;
-            currentPos.row = currentPos.row+1;
-            return currentPos;
-        } else  if (level.checkPassable(currentPos.row,currentPos.col-1)){
-            previousPos = currentPos;
-            currentPos.col = currentPos.col-1;
-            return currentPos;
-        } else {
-            return currentPos;
+    public Position move() {
+            if (level.checkPassable(currentPos.row, currentPos.col + 1)) {
+                previousPos = currentPos;
+                currentPos.col = currentPos.col + 1;
+                return currentPos;
+            } else if (level.checkPassable(currentPos.row - 1, currentPos.col)) {
+                previousPos = currentPos;
+                currentPos.row = currentPos.row - 1;
+                return currentPos;
+            } else if (level.checkPassable(currentPos.row + 1, currentPos.col)) {
+                previousPos = currentPos;
+                currentPos.row = currentPos.row + 1;
+                return currentPos;
+            } else if (level.checkPassable(currentPos.row, currentPos.col - 1)) {
+                previousPos = currentPos;
+                currentPos.col = currentPos.col - 1;
+                return currentPos;
+            } else {
+                return currentPos;
+            }
         }
     }
 
-}
+
